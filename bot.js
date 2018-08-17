4294db5529b94ceca935b08466dfd4b9d8f863b8
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-18 02:46:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,42 +40,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 
 
 
-	let message_handler = {};
-    let tolerancia = [4, 3500]; //4 messages in less than 1200ms = spam
-    module.exports = function(bot, callback){
-      bot.on('message', message => {
-        if(message.author.bot) return;
-        if(!message_handler.hasOwnProperty(message.author.id)){
-          message_handler[message.author.id] = {};
-          message_handler[message.author.id].ultimamessageTS = message.createdTimestamp;
-          message_handler[message.author.id].primeiramessageTS = message.createdTimestamp;
-          message_handler[message.author.id].messageTracker = 0;
-        }
-        var mh = message_handler[message.author.id];
-        mh.messageTracker++;
-        mh.ultimamessageTS = message.createdTimestamp;
-        if(mh.messageTracker >= tolerancia[0] && mh.ultimamessageTS <= (mh.primeiramessageTS + tolerancia[1])){
-            callback(message);
-        } else {
-          setTimeout(function(){
-            delete message_handler[message.author.id];
-            delete mh;
-          }, tolerancia[1]);
-        }
-      });
-    }
-
-
-
-client.on('ready', function(){
-  require("./antispam.js")(client, function(message){
-     message.delete().then(yumz => {
-     message.channel.send(`stop spamming kid <@${message.author.id}>`).then(spammer => {
-     spammer.delete(2000)
-   });
-   });
-  });
-});
+	
 
 
 
